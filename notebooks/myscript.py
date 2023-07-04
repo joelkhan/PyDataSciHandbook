38910aeecf45ac54e2bf65238ecb937fb4eedda6
joelkhan <joelkhan@163.com>
2023-07-04 13:29:42 +0800

    ch01 fin~

--- notebooks/myscript.py
+++ notebooks/myscript.py
@@ -4,3 +4,17 @@ def square(x):
 
 for N in range(1, 4):
     print(N, "squared is", square(N))
+    
+total = 0
+for i in range(10):
+    for j in range(10):
+        total += i*(-1)**j
+        print(total)
+print(total)
+
+def sum_of_lists(N):
+    total = 0
+    for i in range(5):
+        L = [j ^ (j >> i) for j in range(N)]
+        total += sum(L)
+    return total
